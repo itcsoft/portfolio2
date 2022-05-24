0279e6b07a6342b933c2e9eea133397aca0f3700
itcsoft <itcbootcamp@gmail.com>
2022-05-24 17:19:22 +0600

    changed language code, time zone, created super user, added django administration

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import Contact
 
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['email', 'number_of_telephone', 'message', 'sent_at']
+    list_filter = ['sent_at']
+    search_fields = ['email', 'number_of_telephone', 'message', 'sent_at']
+    list_editable = ['number_of_telephone']
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
